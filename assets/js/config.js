e7b976acd349929d238c7c445b8023f631c007fa
lavgreatindiatechnology-wq <lavgreatindiatechnology@gmail.com>
2026-09-03 10:50:25 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,5 @@
-const SUPABASE_URL = "https://ywwojkvarygebbfwiymq.supabase.co";
+const SUPABASE_URL =
+"https://ywwojkvarygebbfwiymq.supabase.co";
 
-const SUPABASE_ANON_KEY = "sb_publishable_uUqW1tX4WdY1we6yj5PnuQ_KpYe_M17";
+const SUPABASE_ANON_KEY =
+"sb_publishable_uUqW1tX4WdY1we6yj5PnuQ_KpYe_M17";
