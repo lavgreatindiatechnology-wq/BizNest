621f4274dc79845e4ac4e128e23aa1aa292f4d93
lavgreatindiatechnology-wq <lavgreatindiatechnology@gmail.com>
2026-09-03 18:18:28 +0530

    Delete assets directory

--- assets/js/supabase-config.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL="https://ywwojkvarygebbfwiymq.supabase.co";
-const SUPABASE_KEY="sb_publishable_uUqW1tX4WdY1we6yj5PnuQ_KpYe_M17";
-const sb=window.supabase.createClient(SUPABASE_URL,SUPABASE_KEY,{
- auth:{persistSession:true,autoRefreshToken:true,detectSessionInUrl:true,storageKey:"biznest-session"}
-});
